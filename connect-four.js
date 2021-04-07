ed901ec309e6729107096e44ddea74a6b686fd7b
gary <baby_plant@icloud.com>
2021-04-07 13:43:25 -0700

    Complete Phase 1

--- connect-four.js
+++ connect-four.js
@@ -3,8 +3,51 @@ import Game from './game.js'
 
 let game = undefined;
 
+const getName = () => {
+
+    return player1.value + player2.value;
+
+
+}
+
+
+
+const updateUI = () => {
+
+    if (game === undefined) {
+
+        const boardHolder = document.getElementById('board-holder')
+        boardHolder.setAttribute('class', 'is-invisible');
+
+    } else {
+
+        //set the inner html gamenameID to 'getName.value'
+
+        const boardHolder = document.getElementById('board-holder')
+        boardHolder.removeAttribute('class', 'is-invisible');
+        const getName = document.getElementById('get-name');
+        getName.innerHTML = getName();
+
+    }
+
+
+
+}
+
+
+
+
+
 window.addEventListener('DOMContentLoaded', (event) => {
 
+    const clickTargets = document.getElementById('click-targets');
+    clickTargets.addEventListener('click', (event) => {
+
+        game.playInColumn();
+        updateUI();
+
+
+    })
 
     const player1 = document.getElementById('player-1-name')
     player1.addEventListener('keyup', (event) => {
@@ -27,9 +70,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     })
     const gameButton = document.getElementById('new-game');
-    gameButton.addEventListener('click', (event) =>{
-        game = new Game(player1.value,player2.value)
-        console.log(game);
+    gameButton.addEventListener('click', (event) => {
+        game = new Game(player1.value, player2.value)
+        player1.value = '';
+        player2.value = '';
+        const gameButton = document.getElementById('new-game')
+        gameButton.disabled = true;
+        updateUI();
+
     })
 
 
--- game.js
+++ game.js
@@ -1,10 +1,18 @@
 export default class Game {
-    constructor(player1Name, player2Name) {
+    constructor(player1Name, player2Name, currentPlayer = 1) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
-
+        this.currentPlayer = currentPlayer;
 
     }
 
+    playInColumn() {
+        if (currentPlayer === 1) {
+            currentPlayer = 2;
+        } else {
+            currentPlayer = 1;
+        }
+    }
 
 }
+
